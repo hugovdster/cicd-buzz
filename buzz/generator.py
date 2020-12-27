78cec9c7cebfa2c1154d7c996c243547b5608114
root <hugo.vanderster@student.hu.nl>
2020-12-27 18:02:03 +0100

    First Changes generator.py

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,18 +1,18 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
-    'continuous deployment', 'continuous improvement', 'devops')
+buzz = ('continuous testing', 'continuous integration', 'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
-adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
-    'seriously')
+adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
+
 
 def sample(l, n = 1):
     result = random.sample(l, n)
     if n == 1:
         return result[0]
     return result
+
 
 def generate_buzz():
     buzz_terms = sample(buzz, 2)
@@ -22,4 +22,3 @@ def generate_buzz():
 
 if __name__ == "__main__":
     print(generate_buzz())
-

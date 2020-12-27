a537b2dcae7f64d68ea4f4a20c3023858293d719
root <hugo.vanderster@student.hu.nl>
2020-12-27 20:54:45 +0100

    Update Flake8 app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from buzz import generator
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def generate_buzz():
     page = '<html><body><h1>'
@@ -11,6 +12,7 @@ def generate_buzz():
     page += 'Greetings from Hugo & Karsten </br>'
     page += '</h1></body></html>'
     return page
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))

c4e47851aa8d1b24efd2a9385467a644816deaf8
root <hugo.vanderster@student.hu.nl>
2020-12-27 17:45:15 +0100

    First Changes test_generator.py

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,18 +1,16 @@
-import unittest
-
 from buzz import generator
 
 def test_sample_single_word():
-    l = ('foo', 'bar', 'foobar')
+    one = ('foo', 'bar', 'foobar')
     word = generator.sample(l)
-    assert word in l
+    assert word in one
 
 def test_sample_multiple_words():
-    l = ('foo', 'bar', 'foobar')
+    one = ('foo', 'bar', 'foobar')
     words = generator.sample(l, 2)
     assert len(words) == 2
-    assert words[0] in l
-    assert words[1] in l
+    assert words[0] in one
+    assert words[1] in one
     assert words[0] is not words[1]
 
 def test_generate_buzz_of_at_least_five_words():
